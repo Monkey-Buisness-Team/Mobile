5b3a2af7b7eea5a6d007996d38b10f14dea40d92
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2024-05-30 00:04:24 +0200

    Texte Defilant HUD

---
+++ Assets/Scripts/UI/HUDManager.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    [Header("Text Defilant")]
+    [SerializeField] List<TextMeshProUGUI> _texts;
+    [SerializeField] Image _background;
+
+    public IEnumerator Start()
+    {
+        for (int i = 0; i < _texts.Count; i++)
+        {
+            StartCoroutine(MoveText(_texts[i]));
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    public void ChangeText(string text) => ChangeText(text, Color.green);
+
+    public void ChangeText(string text, Color color)
+    {
+        for (int i = 0; i < _texts.Count; i++)
+        {
+            _texts[i].text = text;
+        }
+        _background.color = color;
+    }
+
+    IEnumerator MoveText(TextMeshProUGUI text)
+    {
+        RectTransform rectTransform = text.transform as RectTransform;
+
+        while (true)
+        {
+            if (rectTransform.localPosition.x >= 1440f)
+                rectTransform.localPosition = new Vector3(-1440f, rectTransform.localPosition.y);
+            //Debug.Log($"{Screen.width} | {rectTransform.localPosition}");
+            yield return rectTransform.transform.DOLocalMoveX(1440f, 4f).SetEase(Ease.Linear).WaitForCompletion();
+        }
+    }
+}
