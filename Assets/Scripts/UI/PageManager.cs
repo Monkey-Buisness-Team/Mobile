5ed0cc5c0b03df6917af87214476209773b2ff17
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2024-06-01 18:59:04 +0200

    Help Display

--- Assets/Scripts/UI/PageManager.cs
+++ Assets/Scripts/UI/PageManager.cs
@@ -13,11 +13,14 @@ public class PageManager : MonoBehaviour
     {
         public Page Type;
         public GameObject Page;
+        public Sprite HelpImage;
         public Transform StartingTransform { get; set; }
     }
 
     [SerializeField] private List<PageObject> _pages;
     [SerializeField] private Page _currentPage;
+
+    [SerializeField] private Page _lastCasinoPage;
 
     [Header("Casino")]
     [SerializeField] private Transform _casinoTransform;
@@ -26,9 +29,13 @@ public class PageManager : MonoBehaviour
     [SerializeField] private List<Page> _fightPages;
 
     [SerializeField] private GameObject _homeButton;
+    [SerializeField] private GameObject _helpButton;
+    [SerializeField] private Image _helpImageHolder;
 
     public void Start()
     {
+        _lastCasinoPage = Page.Crash;
+
         for (int i = 0; i < _pages.Count; i++)
         {
             _pages[i].StartingTransform = _pages[i].Page.transform;
@@ -52,6 +59,14 @@ public class PageManager : MonoBehaviour
 
         Transform trans = _casinoPages.Contains(_currentPage) ? _casinoTransform : this.transform;
         trans.DOLocalMove(new Vector3(-p.Page.transform.localPosition.x, trans.localPosition.y), 0.5f);
+
+        if (_currentPage == Page.Casino)
+            _currentPage = _lastCasinoPage;
+        else if (_casinoPages.Contains(_currentPage))
+            _lastCasinoPage = _currentPage;
+
+        _helpButton.SetActive(_pages.Find(x => x.Type.ToString().Equals(_currentPage.ToString())).HelpImage != null);
+        _helpImageHolder.gameObject.SetActive(false);
     }
 
     public void GoToPage(string page)
@@ -61,6 +76,14 @@ public class PageManager : MonoBehaviour
 
         Transform trans = _casinoPages.Contains(_currentPage) ? _casinoTransform : this.transform;
         trans.DOLocalMove(new Vector3(-p.Page.transform.localPosition.x, trans.localPosition.y), 0.5f);
+
+        if (_currentPage == Page.Casino)
+            _currentPage = _lastCasinoPage;
+        else if (_casinoPages.Contains(_currentPage))
+            _lastCasinoPage = _currentPage;
+
+        _helpButton.SetActive(_pages.Find(x => x.Type.ToString().Equals(_currentPage.ToString())).HelpImage != null);
+        _helpImageHolder.gameObject.SetActive(false);
     }
 
     public void DisableAllImage()
@@ -72,6 +95,22 @@ public class PageManager : MonoBehaviour
 
         if(_currentPage != Page.Fruit)
             FruitGameManager.i.DeactiveGameOverText(false);
+    }
+
+    public void TryDisplayHelp()
+    {
+        if (_helpImageHolder.isActiveAndEnabled)
+        {
+            _helpImageHolder.gameObject.SetActive(false);
+            return;
+        }
+
+        var page = _pages.Find(x => x.Type.Equals(_currentPage));
+        if (page.HelpImage != null)
+        {
+            _helpImageHolder.sprite = page.HelpImage;
+            _helpImageHolder.gameObject.SetActive(true);
+        }
     }
 
     public void QuitApp()
